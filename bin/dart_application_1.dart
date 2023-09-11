4982c44c385a30e2055a9aad9f6db533ad5fdce7
Fergie Fatah Ardiansyah <fatahardiansyah12@gmail.com>
2023-09-11 14:47:36 +0700

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,50 @@
+
+// void main() {
+//   int nilai = 90;
+
+//   if (nilai > 90) {
+//     print("A");
+//   } else if (nilai > 80) {
+//     print("B");
+//   } else if (nilai > 70) {
+//     print("C");
+//   } else {
+//     print("D");
+//   }
+// }
+
+
+void main() {
+//  var list = [1, 2, 3, 4, 5];
+//  for(var i in list) {
+//   print(i);
+//   }
+  var s = '';
+for (var i = 0; i < 20; i++) {
+  for (var j = 0; j <= i; j++) {
+    s += '😂';
+  }
+  s += '\n';
+}
+
+for (var i = 19; i > 0; i--) {
+  for (var j = 0; j < i; j++) {
+    s += '🤣';
+  }
+  s += '\n';
+}
+print(s);
+}
+
+// String evaluasiNilai(int nilai) {
+//   if (nilai > 90) {
+//     return "A";
+//   } else if (nilai > 80) {
+//     return "B";
+//   } else if (nilai > 70) {
+//     return "C";
+//   } else {
+//     return "D";
+//   }
+// }
+
